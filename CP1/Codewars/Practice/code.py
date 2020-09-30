edd933d366165afa771edba00fe4827af99e07cb
Ian Samir Yep Manzano <isym444@Ians-MacBook-Pro.local>
2020-09-30 12:26:30 +0100

    v0.01

--- CP1/Codewars/Practice/code.py
+++ CP1/Codewars/Practice/code.py
@@ -3,5 +3,5 @@ import sys
 sys.stdout = open("/Users/isym444/Desktop/PythonCP/CP1/Codewars/Practice/output.txt", "w")
 sys.stdin = open("/Users/isym444/Desktop/PythonCP/CP1/Codewars/Practice/input.txt", "r")
 
-a = input()
+a = "hello"
 print(a)
