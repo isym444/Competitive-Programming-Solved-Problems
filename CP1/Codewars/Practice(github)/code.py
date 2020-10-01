da676d6d7ac994bb78f625afffd6a965922d1b18
Ian Samir Yep Manzano <isym444@Ians-MacBook-Pro.local>
2020-10-01 12:50:23 +0100

    codeforces beautiful matrix

--- CP1/Codewars/Practice(github)/code.py
+++ CP1/Codewars/Practice(github)/code.py
@@ -1,7 +1,28 @@
+from itertools import groupby
 import sys
 
-sys.stdout = open("/Users/isym444/Desktop/PythonCP/CP1/Codewars/Practice/output.txt", "w")
-sys.stdin = open("/Users/isym444/Desktop/PythonCP/CP1/Codewars/Practice/input.txt", "r")
 
-a = "hello"
-print(a)
+""" sys.stdout = open("/Users/isym444/Desktop/PythonCP/CP1/Codewars/Practice/output.txt", "w")
+sys.stdin = open("/Users/isym444/Desktop/PythonCP/CP1/Codewars/Practice/input.txt", "r") """
+
+""" input the matrix
+make a dictionary with keys 1->5 and each value as a list of input
+find where 1 is letter[num]
+number of moves to get it to c[2] """
+
+a = {}
+row = 0
+column = 0
+for i in range(1, 6):
+    a[i] = list(map(int, input().split()))
+for i in range(1, 6):
+    if 1 in a[i]:
+        row = i
+        b = a[i]
+counter = 0
+for i in b:
+    counter += 1
+    if i == 1:
+        column = counter
+result = abs(3 - row) + abs(3 - column)
+print(result)
---
+++ CP1/Codewars/Practice/practice.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.stdin = open("/Users/isym444/Desktop/PythonCP/CP1/Codewars/Practice/input.txt", "r")
+sys.stdout = open("/Users/isym444/Desktop/PythonCP/CP1/Codewars/Practice/output.txt", "w")
+
+
+a = int(input())
+print(a)
